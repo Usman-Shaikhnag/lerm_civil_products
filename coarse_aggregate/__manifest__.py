c62e71f61378db89ca2d9bee374e36dd5634c9c5
Lokesh Esehat <lokesh.dalvi@esehat.org>
2025-07-12 12:47:09 +0530

    Brick,Con-Cube, Coa-Agg,SS-TMT-BAR,SSL_bitumen

---
+++ coarse_aggregate/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'COARSE AGGREGATE',
+    'version': '1.2',
+    'category': 'Sales/Sales',
+    'summary': 'Sales internal machinery',
+    'description': """
+This module contains all the common features of Sales Management and eCommerce.
+    """,
+    'depends': ['base','sale','lerm_civil'],
+    'data': [
+                'security/ir.model.access.csv',
+                'views/coarse_agg.xml',
+                'reports/coarse_agg_ds_report.xml'
+    ],
+  
+    'installable': True,
+    'auto_install': False,
+   
+}
