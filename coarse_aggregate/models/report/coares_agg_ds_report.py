76760fdba9481ce2ca2299bf889eb8140653c118
Lokesh Esehat <lokesh.dalvi@esehat.org>
2025-07-16 12:35:05 +0530

    Coarse Aggregate

--- coarse_aggregate/models/report/coares_agg_ds_report.py
+++ coarse_aggregate/models/report/coares_agg_ds_report.py
@@ -54,7 +54,7 @@ class CoarseAggregateReport(models.AbstractModel):
         }
         model = eln.get_product_base_calc_line(data).ir_model.model
         coarse_data = self.env[model].search([("id","=",eln.model_id)])
-        import wdb; wdb.set_trace()
+        # import wdb; wdb.set_trace()
         return {
             'eln': eln,
             'data': coarse_data,
