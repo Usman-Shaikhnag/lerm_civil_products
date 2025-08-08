39cc1d36f6832b9add9f36d94bb819e01d1d06b2
ankitkambl <ankit.kamble@esehat.org>
2025-08-08 11:37:48 +0000

    BRICKS

--- brick/__manifest__.py
+++ brick/__manifest__.py
@@ -17,12 +17,7 @@ This module contains all the common features of Sales Management and eCommerce.
                  'reports/brick_datasheet.xml',
                  'reports/brick_report.xml'
     ],
-    'assets': {
-        'web.assets_backend': [
-            'brick/static/src/css/custom_style.css',
-        ],
-    },
-  
+   
     'installable': True,
     'auto_install': False,
    
