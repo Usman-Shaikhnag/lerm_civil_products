3cabd0e24b5344e36fe7aaa1ee42620d2a54111b
UsmanShaikhnagOfficial <usman.shaikhnag@esehat.org>
2025-09-03 16:15:30 +0530

    reallocate

---
+++ fst/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'FST',
+    'version': '1.2',
+    'category': 'Lerm Civil',
+    'summary': '',
+    'description': """
+    """,
+    'depends': ['base','lerm_civil','soil_resistivity'],
+    'data': [
+                 'views/ert.xml',
+                 'views/soil_resistivity.xml',
+                 'data/sequence.xml',
+                 'security/ir.model.access.csv',
+
+    ],
+  
+    'installable': True,
+    'auto_install': False,
+   
+}
