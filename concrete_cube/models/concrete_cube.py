e6d42382a717127907fe29d7ccc6e2ac867af774
UsmanShaikhnagOfficial <usman.shaikhnag@esehat.org>
2025-12-22 10:18:33 +0000

    Pull

--- concrete_cube/models/concrete_cube.py
+++ concrete_cube/models/concrete_cube.py
@@ -199,6 +199,8 @@ class MechanicalConcreteCube(models.Model):
     def _compute_date_testing(self):
         if self.eln_ref:
             self.date_of_testing = self.eln_ref.date_testing
+        else:
+            self.date_of_testing = self.env['lerm.eln'].sudo().search([('id','=',eln_ref.id)],limit=1)
 
     confirmity = fields.Selection([
         ('pass', 'Pass'),
