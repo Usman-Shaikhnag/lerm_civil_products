8f601f0a5c86b643a4d279ca2669cd3989af9985
ankitkambl <ankit.kamble@esehat.org>
2026-01-05 07:57:44 +0000

    ADD

--- rock/models/rock.py
+++ rock/models/rock.py
@@ -15,7 +15,8 @@ class MechanicalRock(models.Model):
     eln_ref = fields.Many2one('lerm.eln',string="Eln")
     grade = fields.Many2one('lerm.grade.line',string="Grade",compute="_compute_grade_id",store=True)
     size_id = fields.Many2one('lerm.size.line',string="Size",compute="_compute_size_id",store=True)
-    rock_visible = fields.Boolean("rock Test",compute="_compute_visible")
+    
+    # rock_visible = fields.Boolean("rock Test",compute="_compute_visible")
 
     temp_rock = fields.Char(string="Temp.°C" ,required=True)
     humidity_rock= fields.Char(string="Humidity %" ,required=True)
