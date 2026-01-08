85c349839b36179ee4847eb0e4246d5f5298d855
ankitkambl <ankit.kamble@esehat.org>
2026-01-08 02:04:37 +0000

    ADD

--- ggbs/models/ggbs.py
+++ ggbs/models/ggbs.py
@@ -172,7 +172,7 @@ class GgbsMechanical(models.Model):
         domain="[('id', 'in', lab_ggbs_ids)]"
     )
 
-    temp_specific = fields.Char("Temp.°C" ,required=True)
+    temp_specific = fields.Char("Temp.°C" )
     humidity_specific= fields.Char("Humidity %")
 
     temp_water1 = fields.Float("Temperature of Water Bath  when Flask kept in bath – 0C")
@@ -454,8 +454,8 @@ class GgbsMechanical(models.Model):
 
     sai1 = fields.Float(string="Slag Activity Index",digits=(12,2),compute="_compute_slag_activity_index")
 
-    temp_7day = fields.Char("7 Days Temp.°C" ,required=True)
-    humidity_7day= fields.Char("7 Days Humidity %" ,required=True)
+    temp_7day = fields.Char("7 Days Temp.°C" )
+    humidity_7day= fields.Char("7 Days Humidity %" )
 
     day_7_confirmity = fields.Selection([
         ('pass', 'Pass'),
@@ -514,8 +514,8 @@ class GgbsMechanical(models.Model):
                         record.day_7_nabl = 'fail'
 
     sai2 = fields.Float(string="Slag Activity Index",digits=(12,2),compute="_compute_slag_activity_index")
-    temp_28day = fields.Char("28 Days Temp.°C" ,required=True)
-    humidity_28day= fields.Char("28 Days Humidity %" ,required=True)
+    temp_28day = fields.Char("28 Days Temp.°C" )
+    humidity_28day= fields.Char("28 Days Humidity %" )
 
     day_28_confirmity = fields.Selection([
         ('pass', 'Pass'),
@@ -639,8 +639,8 @@ class GgbsMechanical(models.Model):
         domain="[('id', 'in', lab_ggbs_ids)]"
     )
 
-    temp_fineness = fields.Char("Temp.°C" ,required=True)
-    humidity_fineness= fields.Char("Humidity %" ,required=True)
+    temp_fineness = fields.Char("Temp.°C" )
+    humidity_fineness= fields.Char("Humidity %" )
 
     density_cement = fields.Float(string="Density of Cement (g/cc)", digits=(12, 3))
     
