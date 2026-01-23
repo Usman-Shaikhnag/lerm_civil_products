6102152c65b272fe5e772b0270e0861d3b2491a1
lokesh-esehat <lokesh.dalvi@esehat.org>
2026-01-23 15:49:09 +0530

    SAVE

--- fst/models/ert.py
+++ fst/models/ert.py
@@ -665,10 +665,10 @@ class ERTDashboard(models.Model):
     _name = "lerm.ert.dashboard"
     _description = "ERT Dashboard"
 
-    # def search(self, args, offset=0, limit=None, order=None, count=False):
-    # # always show 1 record
-    #     res = super(ERTDashboard, self).search(args, offset=offset, limit=limit, order=order, count=count)
-    #     if not res and not count:
-    #         return self.create({})
-    #     return res
+    def search(self, args, offset=0, limit=None, order=None, count=False):
+    # always show 1 record
+        res = super(ERTDashboard, self).search(args, offset=offset, limit=limit, order=order, count=count)
+        if not res and not count:
+            return self.create({})
+        return res
     
