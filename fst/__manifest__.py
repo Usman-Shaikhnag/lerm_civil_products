7a0e3f32a7fc90e594bbf819f8873c541dc99c35
UsmanShaikhnagOfficial <usman.shaikhnag@esehat.org>
2026-04-17 07:44:30 +0000

    Pull

--- fst/__manifest__.py
+++ fst/__manifest__.py
@@ -21,6 +21,7 @@
                  'views/routine_pullout_pile_load_views.xml',
                  'views/routine_pile_load_test_views.xml',
                  'views/routine_lateral_pile_load_views.xml',
+                 
                  'views/temp_monitoring.xml',
                  'data/sequence.xml',
                  'reports/soil_resistivity_report_docx.xml',
