e446c7ec09ecf69354e458828d9b77f06e23096e
ankitkambl <ankit.kamble@esehat.org>
2026-05-25 01:47:06 +0000

    ADD

---
+++ stack_emission/models/__init__.py
@@ -0,0 +1,2 @@
+from . import stack_emission
+from .report import stack_emission_ds_report
---
+++ stack_emission/models/report/stack_emission_ds_report.py
@@ -0,0 +1,46 @@
+from odoo import models , fields,api
+import json
+import base64
+import qrcode
+from io import BytesIO
+from lxml import etree
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import numpy as np
+import math
+from scipy.interpolate import CubicSpline , interp1d , Akima1DInterpolator
+from scipy.optimize import minimize_scalar
+
+
+
+class StackEmissionDataSheet(models.AbstractModel):
+    _name = 'report.stack_emission.stack_emission_datasheet'
+    _description = 'Stack Emission DataSheet'
+    
+    @api.model
+    def _get_report_values(self, docids, data):
+        if data['fromsample'] == True:
+            if 'active_id' in data['context']:
+                eln = self.env['lerm.eln'].sudo().search([('sample_id','=',data['context']['active_id'])])
+            else:
+                eln = self.env['lerm.eln'].sudo().browse(docids) 
+        else:
+            if data['report_wizard']:
+                if data['report_wizard'] == True:
+                    eln = self.env['lerm.eln'].sudo().search([('id','=',data['eln'])])
+                else:
+                    eln = self.env['lerm.eln'].sudo().browse(data['eln_id'])
+            else:
+                eln = self.env['lerm.eln'].sudo().browse(data['eln_id'])
+        # differnt location for product based
+        # model_name = eln.material.product_based_calculation[0].ir_model.name 
+        model_id = eln.model_id
+        model_name = eln.material.product_based_calculation.filtered(lambda record: record.grade.id == eln.grade_id.id).ir_model.name
+        if model_name:
+            general_data = self.env[model_name].sudo().browse(model_id)
+        else:
+            general_data = self.env['lerm.eln'].sudo().browse(docids)
+        return {
+            'eln': eln,
+            'data' : general_data
+        }
