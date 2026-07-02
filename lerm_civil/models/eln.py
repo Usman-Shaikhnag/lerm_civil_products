8688e39f3999308080a1b840f767ec697b50ee4e
UsmanShaikhnagOfficial <usman.shaikhnag@esehat.org>
2026-07-02 07:21:02 +0000

    PULL

--- lerm_civil/models/eln.py
+++ lerm_civil/models/eln.py
@@ -125,8 +125,6 @@ class ELN(models.Model):
     quantity_received = fields.Integer(string="Quantiyty Received")
     quantity_consumed = fields.Integer(string="Quantity Consumed")
     quantity_balance = fields.Integer(string="Quantity Balance", compute="compute_quantity_balance", readonly=True)
-<<<<<<< HEAD
-<<<<<<< HEAD
     unread_eln = fields.Boolean(string="Unread ELN", default=True)
 
     def action_open_form(self):
@@ -145,10 +143,7 @@ class ELN(models.Model):
     
 
     
-=======
->>>>>>> 177469d900021288c4dbda7bbc53e351f68ad16d
-=======
->>>>>>> 177469d900021288c4dbda7bbc53e351f68ad16d
+
     test_started = fields.Boolean(string="Test Started", default=False)
 
 
@@ -297,13 +292,7 @@ class ELN(models.Model):
     def open_product_based_form(self):
         for record in self:
             # Sample ला target कर
-<<<<<<< HEAD
-<<<<<<< HEAD
-            
-=======
->>>>>>> 177469d900021288c4dbda7bbc53e351f68ad16d
-=======
->>>>>>> 177469d900021288c4dbda7bbc53e351f68ad16d
+
             if (
                 record.state not in ['2-confirm', '3-approved', '4-rejected']
                 and record.sample_id
