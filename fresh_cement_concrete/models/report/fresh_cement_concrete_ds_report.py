b3b16523b37fbf04171d0c431ea320d41e4525f5
HashmiShaikh1 <hashmi.s@esehat.org>
2026-07-09 12:58:48 +0530

    Fine Aggregate

---
+++ fresh_cement_concrete/models/report/fresh_cement_concrete_ds_report.py
@@ -0,0 +1,104 @@
+from odoo import models , fields,api
+import json
+import base64
+import qrcode
+from io import BytesIO
+from lxml import etree
+
+
+
+
+class FreshCementConcreteReport(models.AbstractModel):
+    _name = 'report.fresh_cement_concrete.fresh_cement_concrete_report'
+    _description = 'Fresh Cement Concrete Report'
+    
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        data = data or {}
+        nabl = data.get('nabl', False)
+
+        # 🧩 ELN Record मिळवा
+        if data.get('report_wizard'):
+            eln = self.env['lerm.eln'].sudo().search([('sample_id', '=', data.get('sample'))])
+        elif 'active_id' in data.get('context', {}):
+            eln = self.env['lerm.eln'].sudo().search([('sample_id', '=', data['context']['active_id'])])
+        else:
+            eln = self.env['lerm.eln'].sudo().browse(docids)
+
+        if not eln:
+            raise ValueError("ELN record not found")
+
+        # Static QR
+        qr_static = qrcode.QRCode(box_size=6, border=2)
+        qr_static.add_data("https://www.lerm.in")
+        qr_static.make(fit=True)
+        buf_static = BytesIO()
+        qr_static.make_image(fill_color="black", back_color="white").save(buf_static, format="PNG")
+        qr_static_b64 = base64.b64encode(buf_static.getvalue()).decode()
+
+        # 🧩 QR Code तयार करा
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
+        report_url = f"{base_url}/download_report/aac/{'nabl' if nabl else 'nonnabl'}/{eln.id}"
+
+        qr.add_data(report_url)
+        qr.make(fit=True)
+        qr_image = qr.make_image()
+        buffered = BytesIO()
+        qr_image.save(buffered, format="PNG")
+        qr_code = base64.b64encode(buffered.getvalue()).decode()
+
+         # ✅ General Data मिळवा
+        model_id = eln.model_id
+        model_name = (
+            eln.material.product_based_calculation[0].ir_model.name
+            if eln.material.product_based_calculation else False
+        )
+        if model_name:
+            general_data = self.env[model_name].sudo().browse(model_id)
+        else:
+            general_data = self.env['lerm.eln'].sudo().browse(docids)
+        
+        return {
+            'eln': eln,
+            'data' : general_data,
+            'qrcode': qr_code,
+            'nabl' : nabl,
+            'qrcode_static': qr_static_b64,
+            # 'stamp' : inreport_value,
+            'nabl' : nabl
+        }
+
+class FreshCementConcreteDatasheet(models.AbstractModel):
+    _name = 'report.fresh_cement_concrete.fresh_cement_concrete_datasheet'
+    _description = 'Fresh Cement Concrete DataSheet'
+    
+    @api.model
+    def _get_report_values(self, docids, data):
+        if data['fromsample'] == True:
+            if 'active_id' in data['context']:
+                eln = self.env['lerm.eln'].sudo().search([('sample_id','=',data['context']['active_id'])])
+            else:
+                eln = self.env['lerm.eln'].sudo().browse(docids) 
+        else:
+            if data['report_wizard'] == True:
+                eln = self.env['lerm.eln'].sudo().search([('id','=',data['eln'])])
+            else:
+                eln = self.env['lerm.eln'].sudo().browse(data['eln_id'])
+        model_id = eln.model_id
+        # differnt location for product based
+        # model_name = eln.material.product_based_calculation[0].ir_model.name 
+        model_name = eln.material.product_based_calculation.filtered(lambda record: record.grade.id == eln.grade_id.id).ir_model.name
+        if model_name:
+            general_data = self.env[model_name].sudo().browse(model_id)
+        else:
+            general_data = self.env['lerm.eln'].sudo().browse(docids)
+        return {
+            'eln': eln,
+            'data' : general_data
+        }
