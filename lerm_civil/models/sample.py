96aac55e91af154636cd500d9d4500b9a5722d82
ankitkambl <ankit.kamble@esehat.org>
2026-07-20 02:05:23 +0000

    ADD

--- lerm_civil/models/sample.py
+++ lerm_civil/models/sample.py
@@ -196,6 +196,9 @@ class LermSampleForm(models.Model):
             'target': 'current',
         }
 
+
+    
+
         
     @api.depends('quantity_received', 'quantity_consumed','quantity_discarded')
     def compute_quantity_balance(self):
