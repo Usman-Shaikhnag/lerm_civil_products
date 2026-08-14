0ab5f1a90166d655ea0f23f7dcf2b103b080d19f
ankitkambl <ankit.kamble@esehat.org>
2026-08-14 03:42:52 +0000

    ADD

--- paver_block/models/report/paver_ds_report.py
+++ paver_block/models/report/paver_ds_report.py
@@ -40,55 +40,6 @@ class PaverBlockDatasheet1(models.AbstractModel):
                 'parameter' : parameter_data
             }
         
-# class PaverBlockReport1(models.AbstractModel):
-#     _name = 'report.paver_block.paver_block_report_ssl'
-#     _description = 'Paver Block Report 1'
-    
-#     @api.model
-#     def _get_report_values(self, docids, data):
-#         # eln = self.env['lerm.eln'].sudo().browse(docids)
-#         inreport_value = data.get('inreport', None)
-#         nabl = data.get('nabl')
-#         if 'active_id' in data['context']:
-#             eln = self.env['lerm.eln'].sudo().search([('sample_id','=',data['context']['active_id'])])
-#         else:
-#             eln = self.env['lerm.eln'].sudo().browse(docids) 
-
-#         qr_static = qrcode.QRCode(box_size=6, border=2)
-#         qr_static.add_data("https://www.lerm.in")
-#         qr_static.make(fit=True)
-#         buf_static = BytesIO()
-#         qr_static.make_image(fill_color="black", back_color="white").save(buf_static, format="PNG")
-#         qr_static_b64 = base64.b64encode(buf_static.getvalue()).decode()
-
-#         qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
-#         qr.add_data(eln.kes_no)
-#         qr.make(fit=True)
-#         qr_image = qr.make_image()
-
-#         # Convert the QR code image to base64 string
-#         buffered = BytesIO()
-#         qr_image.save(buffered, format="PNG")
-#         qr_image_base64 = base64.b64encode(buffered.getvalue()).decode()
-
-#         # Assign the base64 string to a field in the 'srf' object
-#         qr_code = qr_image_base64
-#         model_id = eln.model_id
-#         # differnt location for product based
-#         model_name = eln.material.product_based_calculation[0].ir_model.name 
-#         if model_name:
-#             general_data = self.env[model_name].sudo().browse(model_id)
-#         else:
-#             general_data = self.env['lerm.eln'].sudo().browse(docids)
-#         return {
-#             'eln': eln,
-#             'data' : general_data,
-#             'qrcode': qr_code,
-#             'qrcode_static': qr_static_b64,
-#             'stamp' : inreport_value,
-#             'nabl' : nabl
-#         }
-
 
 
 class PaverBlockReport1(models.AbstractModel):
@@ -98,26 +49,45 @@ class PaverBlockReport1(models.AbstractModel):
     @api.model
     def _get_report_values(self, docids, data=None):
         data = data or {}
-        nabl = data.get('nabl', False)
+        nabl = data.get("nabl", False)
 
-        # 🧩 ELN Record मिळवा
-        if data.get('report_wizard'):
-            eln = self.env['lerm.eln'].sudo().search([('sample_id', '=', data.get('sample'))])
-        elif 'active_id' in data.get('context', {}):
-            eln = self.env['lerm.eln'].sudo().search([('sample_id', '=', data['context']['active_id'])])
+        # ✅ ELN Fetch
+        if data.get("report_wizard"):
+            eln = (
+                self.env["lerm.eln"]
+                .sudo()
+                .search([("sample_id", "=", data.get("sample"))], limit=1)
+            )
+        elif data.get("context", {}).get("active_id"):
+            eln = (
+                self.env["lerm.eln"]
+                .sudo()
+                .search([("sample_id", "=", data["context"]["active_id"])], limit=1)
+            )
         else:
-            eln = self.env['lerm.eln'].sudo().browse(docids)
+            eln = self.env["lerm.eln"].sudo().browse(docids)
 
-        if not eln:
+        if not eln or not eln.exists():
             raise ValueError("ELN record not found")
 
-        # Static QR
-        qr_static = qrcode.QRCode(box_size=6, border=2)
-        qr_static.add_data("https://www.lerm.in")
-        qr_static.make(fit=True)
-        buf_static = BytesIO()
-        qr_static.make_image(fill_color="black", back_color="white").save(buf_static, format="PNG")
-        qr_static_b64 = base64.b64encode(buf_static.getvalue()).decode()
+        # ✅ LAB FETCH
+        lab = eln.sample_id.lab_location if eln.sample_id else False
+
+        # ✅ QR LINK (थेट NABL ची मूळ लिंक QR मध्ये टाकणे)
+        qr_link = lab.nabl_scope_link or ""
+
+        qrcode_static = False  # <--- हे नाव खाली return मध्ये वापरले आहे
+        if qr_link:
+            # 🔳 QR Generate (NABL च्या लिंकचा QR)
+            qr = qrcode.QRCode(box_size=6, border=2)
+            qr.add_data(qr_link)
+            qr.make(fit=True)
+
+            buffer = BytesIO()
+            qr.make_image(fill_color="black", back_color="white").save(
+                buffer, format="PNG"
+            )
+            qrcode_static = base64.b64encode(buffer.getvalue()).decode()
 
         # 🧩 QR Code तयार करा
         qr = qrcode.QRCode(
@@ -152,7 +122,6 @@ class PaverBlockReport1(models.AbstractModel):
             'data' : general_data,
             'qrcode': qr_code,
             'nabl' : nabl,
-            'qrcode_static': qr_static_b64,
-            # 'stamp' : inreport_value,
-            'nabl' : nabl
+            "qrcode_static": qrcode_static,
+            
         }
