90739f8af286929cc260253eebb7540ec4caca57
Usman-Shaikhnag <ushaikhnag@gmail.com>
2026-08-21 18:27:12 +0530

    flyash

--- fly_ash/models/fly_ash.py
+++ fly_ash/models/fly_ash.py
@@ -1891,16 +1891,17 @@ class SoundnessAutoclaveMethod(models.Model):
 
     sr_no = fields.Integer(string="Sr No.",readonly=True, copy=False, default=1)
 
-    intial_length = fields.Float("Intial Length (L1)",digits=(12,3))
-    final_length = fields.Float("Final Length (L2)",digits=(12,3))
-    expantion = fields.Float("Expansion ((L2-L1)/L1)x 100, %",compute="_compute_expansion_opc",digits=(12,3))
+    intial_length = fields.Float("Length of Bar after Immersion in Water (L1)",digits=(12,2))
+    final_length = fields.Float("Final Length (L2)",digits=(12,2))
+    original_length = fields.Float("Original Length",digits=(12,2)) #just for display
+    expantion = fields.Float("Expansion (L2-(L1/L2))x 100, %",compute="_compute_expansion_opc",digits=(12,2))
 
-
+    # (L2-(L1/L2))x 100
     @api.depends('intial_length', 'final_length')
     def _compute_expansion_opc(self):
         for record in self:
-            if record.intial_length:  # Avoid division by zero
-                record.expantion = ((record.final_length - record.intial_length) / record.intial_length) * 100
+            if record.final_length:  # Avoid division by zero (L2 is denominator)
+                record.expantion = (record.final_length - (record.intial_length / record.final_length)) * 100
             else:
                 record.expantion = 0.0
 
