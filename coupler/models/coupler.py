6addd02131980994d90cfb9a939e983729200743
Usman-Shaikhnag <ushaikhnag@gmail.com>
2026-08-25 08:37:12 +0000

    ADD

--- coupler/models/coupler.py
+++ coupler/models/coupler.py
@@ -236,7 +236,7 @@ class MechanicalCoupler(models.Model):
     @api.model
     def create(self, vals):
         # import wdb;wdb.set_trace()
-        record = super(CouplerLine, self).create(vals)
+        record = super(MechanicalCoupler, self).create(vals)
         # record.get_all_fields()
         record.eln_ref.write({'model_id':record.id})
         return record
