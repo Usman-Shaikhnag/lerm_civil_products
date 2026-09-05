274dca9524f838e5490c544c4352f3e0f6932907
Usman-Shaikhnag <ushaikhnag@gmail.com>
2026-09-05 04:51:38 +0000

    ADD

---
+++ gsb1/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'GSB',
+    'version': '1.2',
+    'category': 'LERM CIVIL',
+    'summary': 'GSB',
+    'description': """
+This module contains all the common features of GSB.
+    """,
+    'depends': ['base','lerm_civil'],
+    'data': [
+                 'security/ir.model.access.csv',
+                 'views/gsb.xml',
+                 'reports/gsb_datasheet.xml',
+                 'reports/gsb_report.xml'
+    ],
+
+   
+  
+    'installable': True,
+    'auto_install': False,
+   
+}
---
+++ gsb1/models/__init__.py
@@ -0,0 +1,3 @@
+from . import gsb1
+from .report import gsb_ds_report
+from . import prefill_data_wizard
