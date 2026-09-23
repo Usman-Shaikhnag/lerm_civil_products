2e9bae7788015309db513ed4ef9e4e3dcb03200e
Usman-Shaikhnag <ushaikhnag@gmail.com>
2026-09-23 07:42:25 +0000

    pull

--- concrete_core/models/concrete_core.py
+++ concrete_core/models/concrete_core.py
@@ -14,11 +14,21 @@ class ConcreteCore(models.Model):
     average = fields.Float(string="Average Compressive Strength in Mpa",compute="_compute_average")
     structure = fields.Char("Structure")
     grade = fields.Many2one('lerm.grade.line',string="Grade",compute="_compute_grade_id",store=True)
+    sample_parameters = fields.Many2many('lerm.parameter.master',string="Parameters",compute="_compute_sample_parameters",store=True)
 
 
     notes = fields.One2many('mechanical.concrete.core.notes','parent_id',string="Notes")
     eln_ref = fields.Many2one('lerm.eln',string="Eln")
     eln_state = fields.Selection(related='eln_ref.state', string="ELN State", store=True)
+
+
+    @api.depends('eln_ref')
+    def _compute_sample_parameters(self):
+
+        for record in self:
+            records = record.eln_ref.parameters_result.parameter.ids
+            record.sample_parameters = records
+            print("Records",records)
 
     def open_eln_page(self):
         # parameter_based_assignment
